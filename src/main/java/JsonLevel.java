9d817c12e1f982bba5f768ab93a736f3eacf2d7b
s0th77 <mihai_deviant@yahoo.com>
2015-05-02 15:39:38 +0300

    Reformat json level class.

--- src/main/java/JsonLevel.java
+++ src/main/java/JsonLevel.java
@@ -2,10 +2,16 @@
  * Created by mike on 2/21/2015.
  */
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.FileWriter;
-import org.json.*;
 
 public class JsonLevel {
+    private static final String TIMER = new String("moveLimit");
+    private static final String MOVES = new String("timeLimit");
+    private static final String EXPLOSION = new String("pepperCandyExplosionTurns");
+
     private JSONObject _level;
 
     public JsonLevel(String jsonString) {
@@ -20,22 +26,26 @@ public class JsonLevel {
         try {
             int value = _level.getInt(elementName);
             System.out.println(elementName + ": " + value);
-            value = value*2 + 15;
+            value = value*3 + 15;
             _level.put(elementName, value);
         }
         catch(JSONException jEx) {
-
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
     public void modifyTimeLimit() {
-        modifyElement("timeLimit");
+        modifyElement(TIMER);
     }
 
     public void modifyNoMoves() {
-        modifyElement("moveLimit");
+        modifyElement(MOVES);
+    }
+
+    public void modifyExplosionTimer() {
+        modifyElement(EXPLOSION);
     }
 
     public void writeJSON(String filePath) {
